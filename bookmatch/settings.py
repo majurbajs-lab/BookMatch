2bded9a311db469930fe46eaa15e681670907dbd
majurbajs-lab <majurbajs@gmail.com>
2026-05-12 18:54:47 +0200

    Add PythonAnywhere to ALLOWED_HOSTS

--- bookmatch/settings.py
+++ bookmatch/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookmatch.pythonanywhere.com']
 
 
 INSTALLED_APPS = [
