5c9ab5ac1617ac52eecec190e567f99a149ec601
majurbajs-lab <majurbajs@gmail.com>
2026-05-12 18:56:52 +0200

    Switch to SQLite for PythonAnywhere free tier

--- bookmatch/settings.py
+++ bookmatch/settings.py
@@ -67,16 +67,8 @@ WSGI_APPLICATION = 'bookmatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
